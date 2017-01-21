368980ba5a9f0fbf4cda77048eb37e8812c8e63f
Alexander Davis <Alexander Davis>
2017-01-20 20:50:26 -1000

    Thank you for the help Vic!

--- js/app.js
+++ js/app.js
@@ -32,20 +32,40 @@ profileElement[2].innerHTML = "And you will know My name is the Lord when I lay 
 /*6. Create a div element for Chuck Norris and give it an id of "name7". Inside this div element, give it the contents of "Chuck Norris".
 
 Append this div element to the nameBox div*/
-var name7 = document.createElement("string");
-name7.id = "norrisName";
-name7.innerHTML = "Chuck Norris";
-nameBox.appendChild(name7);
- 
+var newChuck = document.createElement("div");
+newChuck.id = "name7";
+newChuck.innerHTML = "Chuck Norris";
+nameBox.appendChild(newChuck);
 
 /*7. Create a div element for Grimmace and give it an id of "alias8". Inside this div element, give it the contents of "The Purple Monster".
 
 Append this div element to the aliasBox div.*/
 
-var alias8 = document.createElement("string");
-alias8.id = "colorMonster";
+var alias8 = document.createElement("div");
+alias8.id = "alias8";
 alias8.innerHTML = "Purple Monster";
 aliasBox.appendChild(alias8);
 
 //Final Boss
 /*8. Create your own profile.*/
+
+var newName = document.createElement("div");
+var newPosition = document.createElement("div");
+var newAlias = document.createElement("div");
+var newBio = document.createElement("div");
+
+newName.className = "name";
+newPosition.className = "position";
+newAlias.className = "alias";
+newBio.className = "profile";
+
+newName.innerHTML = "Dakota";
+newPosition.innerHTML = "Software Engineer";
+newAlias.innerHTML = "Javascript Messiah";
+newBio.innerHTML = "lorem ipsum";
+
+var newProfile = document.getElementsByClassName("block3 col-sm-4")[2];
+newProfile.appendChild(newName);
+newProfile.appendChild(newPosition);
+newProfile.appendChild(newAlias);
+newProfile.appendChild(newBio);
